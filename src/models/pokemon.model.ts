2a564f53cd4b9a0293dddb8a26158ec00ec2c6dc
Gabriela Zdunek <gabrielazdunek@icloud.com>
2021-09-27 15:29:32 +0200

    first-commit

---
+++ src/models/pokemon.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Pokemon extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  category: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  type: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  abilities: string;
+
+  @property({
+    type: 'string',
+  })
+  evolution?: string;
+
+
+  constructor(data?: Partial<Pokemon>) {
+    super(data);
+  }
+}
+
+export interface PokemonRelations {
+  // describe navigational properties here
+}
+
+export type PokemonWithRelations = Pokemon & PokemonRelations;
